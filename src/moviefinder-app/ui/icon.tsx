3ac0b3e8ede0ead76aaec15990e03e56cefd2fe2
Chris Vouga <crvouga@gmail.com>
2024-09-06 20:57:17 -0700

    Refactor icon component and remove unused icons

---
+++ src/moviefinder-app/ui/icon.tsx
@@ -0,0 +1,24 @@
+export const IconSpinner = () => {
+  return `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        class="size-12"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12 19a7 7 0 100-14 7 7 0 000 14zm0 3c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+          clip-rule="evenodd"
+          opacity="0.2"
+        />
+        <path d="M2 12C2 6.477 6.477 2 12 2v3a7 7 0 00-7 7H2z" />
+      </svg>`;
+};
+
+export const IconArrowLeft = () => {
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-7">
+        <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+    </svg>`;
+};
