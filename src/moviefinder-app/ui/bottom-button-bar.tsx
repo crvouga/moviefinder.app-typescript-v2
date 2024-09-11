2f370f741132be246a7de60e88c777aac1a8de46
Chris Vouga <crvouga@gmail.com>
2024-09-11 01:48:44 -0700

    Refactor BottomButtonBar component to use button elements instead of anchor elements

--- src/moviefinder-app/ui/bottom-button-bar.tsx
+++ src/moviefinder-app/ui/bottom-button-bar.tsx
@@ -14,25 +14,18 @@ export const BottomButtonBar = (input: {
   return (
     <div class="flex w-full items-center divide-x border-t" data-loading-states>
       {input.buttons.map((button) => (
-        <a
+        <button
           hx-get={button.hxGet}
           hx-target={button.hxTarget}
           hx-push-url="true"
           class={cn(
-            "relative flex flex-1 cursor-pointer flex-col items-center justify-center gap-0.5 py-3 text-xs",
+            "group relative flex flex-1 cursor-pointer flex-col items-center justify-center gap-0.5 py-3 text-xs",
             button.active ? "text-blue-500" : "",
           )}
-          data-loading-aria-busy
-          data-loading-disable
         >
-          <div class="pointer-events-none absolute inset-0 flex items-center justify-center opacity-0 group-aria-busy:opacity-100">
-            <Spinner class="size-8" />
-          </div>
-          <div class="relative flex flex-1 cursor-pointer flex-col items-center justify-center gap-0.5 py-3 text-xs">
-            {button.icon}
-            {button.text}
-          </div>
-        </a>
+          {button.icon}
+          {button.text}
+        </button>
       ))}
     </div>
   );
