6fa32d24285d89eb46b7d04ec83e14e92d18d864
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-10-01 14:49:39 -0700

    Refactor database connection implementation to use PostgreSQL instead of generic SQL

--- src/moviefinder-app/fixture.ts
+++ src/moviefinder-app/fixture.ts
@@ -28,7 +28,7 @@ export const BaseFixture = async () => {
   const sleep = async () => {};
 
   const dbConnSql = await DbConnSql({
-    t: "pg",
+    t: "postgres",
     databaseUrl: DATABASE_URL,
     logger,
   });
